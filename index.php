3119433d0f313b87b7ef04d684ff52226208885f
kostya-nik-a <kostya.nik.a@yandex.ru>
2018-03-12 00:07:34 +0700

    cm

--- index.php
+++ index.php
@@ -10,14 +10,6 @@ try {
     $connect = DB_DRIVER . ':host='. DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8';
     $db = new PDO($connect, DB_USER,DB_PASS);
 
-    CREATE TABLE `tasks` (
-        `id` int(11) NOT NULL AUTO_INCREMENT,
-        `description` text NOT NULL,
-        `is_done` tinyint(4) NOT NULL DEFAULT '0',
-        `date_added` datetime NOT NULL,
-        PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
     $selectTasks = $db->query("SELECT * FROM tasks");
 
     $taskName = "addTask";
